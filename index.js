e54638bd88b965e202148a190a59db6c5c7092e3
royperalta <rrpb33@gmail.com>
2022-04-17 23:01:18 -0500

    nuevos cambios

--- index.js
+++ index.js
@@ -1,54 +1,102 @@
 const { chromium } = require('playwright')
 const links = require('./modules/getData')
 const axios = require('axios')
+const { MongoClient } = require('mongodb')
+
+const uri = "mongodb://127.0.0.1:27017"
+const client = new MongoClient(uri)
 
 async function validar() {
-    const getLinks = await links('https://www.youtube.com/c/CorazonSerranoTV/videos');
-    console.log(getLinks)
+    try{
+        const getLinks = await links('https://www.youtube.com/c/CorazonSerranoTV/videos');
+        console.log(getLinks)
+    
+        await client.connect()
+        const database = client.db("youtube")
+        const datos = database.collection("datos")
+        const arr = []
+        await Promise.all(
+            getLinks.map(async link => {
+                const query = { "code": `${link}` }
+                const dato = await datos.findOne(query)
+                if (dato == null) {
+                    arr.push(link)
+                }
+            })
+        )
+    
+        arr.map((link, index) => {
+            setTimeout(() => {
+                descargar(link)
+            }, 60000 * index)
+        })   
 
-    getLinks.map((data,index)=>{
-        setTimeout(()=>{
-             descargar(data)
-        },60000*index)    
-    })
-}
+    }catch(e){
+        console.log(e)
+    }
+   
 
 
+    /* getLinks.map(async (link, index) => {
+        setTimeout(async () => {
+            await client.connect()
+            const database = client.db("youtube")
+            const datos = database.collection("datos")
+            const query = { "code": `${link}` }
+            const dato = await datos.findOne(query)
+            console.log(dato)
+            if (dato == null) {                
+                    descargar(link)              
+            } else {
+                console.log("Ya esta descargado")
+            }
+        }, 60000 * index)
 
-
-async function descargar(link){try{
-    console.log(link)
-    const browser = await chromium.launch({ headless: false });
-    const page = await browser.newPage({
-        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
-        timeout: 0
-    })
-    await page.goto('https://yoump3.app/es4')
-    await page.locator('input[type="url"]').fill("https://youtube.com" + link)
-    await page.waitForTimeout(3000)
-    await page.locator('input[type="submit"]').click()
-    await page.waitForTimeout(3000)
-    await page.locator('[class="button accept"]').click()  
-    await page.waitForTimeout(3000)  
-
-    /*  const re = await page.click('[class="button button-download"]')
-     console.log(re)
-     console.log('encontrado') */
-    const [download] = await Promise.all([
-        page.waitForEvent('download'),
-        page.click('[class="button button-download"]')
-    ]);
-    console.log(download._url)
-    console.log(download._suggestedFilename)
-    await download.saveAs(`E:\\${download._suggestedFilename}`)
-  /*   await axios.post('http://localhost:5000/api/post',{
-        code:link,
-        name:download._suggestedFilename
     }) */
-    page.close()
-}catch(e) {
-    console.log(e)
+
+
+    /*  getLinks.map((data, index) => {
+         setTimeout(() => {
+             //descargar(data)
+         }, 60000 * index)
+     }) */
 }
+
+
+async function descargar(link) {
+    try {
+        console.log(link)
+        const browser = await chromium.launch({ headless: false });
+        const page = await browser.newPage({
+            userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36',
+            timeout: 0
+        })
+        await page.goto('https://yoump3.app/es4')
+        await page.locator('input[type="url"]').fill("https://youtube.com" + link)
+        await page.waitForTimeout(3000)
+        await page.locator('input[type="submit"]').click()
+        await page.waitForTimeout(3000)
+        await page.locator('[class="button accept"]').click()
+        await page.waitForTimeout(3000)
+
+        /*  const re = await page.click('[class="button button-download"]')
+         console.log(re)
+         console.log('encontrado') */
+        const [download] = await Promise.all([
+            page.waitForEvent('download'),
+            page.click('[class="button button-download"]')
+        ]);
+        console.log(download._url)
+        console.log(download._suggestedFilename)
+        await download.saveAs(`E:\\${download._suggestedFilename}`)
+        await axios.post('http://localhost:5000/api/post', {
+            code: link,
+            name: download._suggestedFilename
+        })
+        page.close()
+    } catch (e) {
+        console.log(e)
+    }
 }
 
 //descargar()
